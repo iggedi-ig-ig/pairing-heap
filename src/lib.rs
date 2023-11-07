3781036229143b1db074011bb1e7e859893c8a38
igs <iggedi@protonmail.com>
2023-11-07 22:57:21 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
-use std::ops::Deref;
-
 use derive_more::Deref;
+use thiserror::Error;
+
+/// This is what traits a type must implement so it can be used as a key of a [`PairingHeap`].
 pub trait Comparable: Copy + Clone + PartialEq + Eq + PartialOrd + Ord {}
 
+/// A Pairing Heap is an addressable priority queue data structure.
+/// It implements the following operations with their (amortized) time complexities:
+///
+/// # Operations
+/// ## Insert - O(1)
+/// Inserts a new element to the heap, returning a handle to it.
+///
+/// ## Remove - O(log n)
+/// Removes an element from the heap, given a handle to it.
+///
+/// ## Merge - O(1)
+/// Merges two [`PairingHeap`]s into one larger [`PairingHeap`]
+///
+/// ## Get Min - O(1)
+/// Returns the smallest element from the heap
+///
+/// ## Pop Min - O(log n)
+/// Returns the smallest element from the heap and removes it, updating its state accordingly
+///
+/// ## Decrease Key - Exact Complexity unknown, between O(log log n) and O(log n)
+/// Decreases the priority of an element from the heap, given a pointer to it, and the new key.
 pub struct PairingHeap<T>
 where
     T: Comparable,
@@ -11,6 +33,7 @@ where
     min_ptr: Option<*mut PairingHeapNode<T>>,
 }
 
+#[derive(Deref)]
 pub struct Handle<T>(*mut PairingHeapNode<T>)
 where
     T: Comparable;
@@ -24,7 +47,13 @@ where
     }
 
     pub fn decrease_key(&mut self, h: Handle<T>, new_key: T) {
-
+        unsafe {
+            let old_key = ***h;
+            if old_key < new_key {
+                // TODO: return Err()
+                panic!("Invalid argument: new_key must be smaller than the old key.")
+            }
+        }
     }
 
     fn new_tree(&mut self, item: T) -> Handle<T> {
@@ -56,9 +85,7 @@ where
         Handle(new_root)
     }
 
-    fn cut(&mut self, h: Handle<T>) {
-        
-    }
+    fn cut(&mut self, h: Handle<T>) {}
 }
 
 #[derive(Deref)]
@@ -85,4 +112,11 @@ where
             one_child: None,
         }
     }
+}
+
+/// This enum represents the possilbe errors that can occurr in an invocation of `decrease_key` on a [`PairingHeap`].
+#[derive(Error, Debug)]
+pub enum DecreaseKeyError {
+    #[error("The new key was larger than the original key.")]
+    KeyLarger,
 }
